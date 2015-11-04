27c37da9ecf60e89e62465aeec601c0e0d1ab8bc
Gahoolee <gahoolee@GahooleedeMacBook-Pro.local>
2015-11-04 23:03:30 +0800

    add symbol, cgc

--- RG.R
+++ RG.R
@@ -2,6 +2,21 @@ library(plyr)
 library(dplyr)
 library(tidyr)
 library(magrittr)
+library(org.Hs.eg.db)
+
+loadCGCsymbols<-function(){
+  cgc<-read.delim('cancer_gene_census_2012_0315', sep='\t', as.is=T)
+  gene_alias<-as.data.frame(org.Hs.egALIAS2EG)
+  gene_alias_symbol<-merge(gene_alias, gene_symbol)
+  alias<-setdiff(cgc$Symbol, gene_symbol$symbol)
+  alias_idx<-gene_alias_symbol$alias_symbol %in% alias
+  #gene_idx<-gene_alias_symbol$gene_id %in% cgc$GeneID
+  #cgc_symbols<-c(cgc$Symbol, gene_alias_symbol[gene_idx&alias_idx,]$symbol)
+  c(cgc$Symbol, gene_alias_symbol[alias_idx,]$symbol)
+}
+
+gene_symbol<-as.data.frame(org.Hs.egSYMBOL)
+cgc_symbols<-loadCGCsymbols()
 
 dir('CIRI','*.CIRI')
 samples<-c('S33N', 'S33T', 'S33NT')
@@ -46,7 +61,12 @@ ciri_df_RG<-merge(ciri_df, S33NT_RG, by='circRNA_ID') %>%
          isNTjunctionEqNTtotal = S33NT_X.junction_reads==NT_total,
          NTjunctionDiffRGtotal = S33NT_X.junction_reads - RG_total,
          NTtotalDiffRGtotal = NT_total - RG_total,
-         NTjunctionDiffNTtotal = S33NT_X.junction_reads - NT_total
-         )
+         NTjunctionDiffNTtotal = S33NT_X.junction_reads - NT_total,
+         transcripts = gene_id,
+         gene_id =  gsub(':.*$','',gene_id),
+         gene_id =  gsub('^n/a$','',gene_id)
+         ) %>%
+  merge(gene_symbol, by='gene_id', all.x=T) %>%
+  mutate(inCGC = symbol %in% cgc_symbols)
 
 write.table(ciri_df_RG, file='ciri_RG.txt', row.names=F, sep='\t', quote=F)
